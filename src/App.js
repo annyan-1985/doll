c2943bcca877c0b5ce9d5995f3a11c7caa902278
Ann Yan <ann.yan.1985@gmail.com>
2023-05-13 11:02:32 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import "./styles.css"; // Import the CSS file
+
+function App() {
+  const [selectedClothing, setSelectedClothing] = useState(null);
+
+  const handleClothingClick = (clothing) => {
+    setSelectedClothing(clothing);
+  };
+
+  return (
+    <div>
+      <Doll selectedClothing={selectedClothing} />
+      <ClothList handleClothingClick={handleClothingClick} />
+      <SelectedClothing clothing={selectedClothing} />
+    </div>
+  );
+}
+
+function Doll({ selectedClothing }) {
+  const dollImageSrc = "doll.jpg";
+
+  return (
+    <div className="doll-container">
+      <img className="doll-image" src={dollImageSrc} alt="Doll" />
+      {selectedClothing && (
+        <span className="selected-clothing">({selectedClothing})</span>
+      )}
+    </div>
+  );
+}
+
+function ClothList({ handleClothingClick }) {
+  const items = [
+    { id: 1, name: "Uniform1", description: "This is item 1." },
+    { id: 2, name: "Uniform2", description: "This is item 2." },
+    { id: 3, name: "Uniform3", description: "This is item 3." },
+  ];
+
+  const handleButtonClick = (name) => {
+    handleClothingClick(name);
+  };
+
+  return (
+    <div>
+      {items.map((item) => (
+        <div key={item.id}>
+          <button
+            dangerouslySetInnerHTML={{ __html: item.name }}
+            onClick={() => handleButtonClick(item.name)}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function SelectedClothing({ clothing }) {
+  return <div>Selected Clothing: {clothing}</div>;
+}
+
+export default App;
