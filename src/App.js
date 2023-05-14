e97d067703d13f8a97d6e1e1daff87ae85df7599
Zikai Zhang <zikai.zhang@data61.csiro.au>
2023-05-14 16:27:57 +1000

    add category

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import "./styles.css"; // Import the CSS file
 import ItemSet from "./ItemSet";
+import { Layout, Space, FloatButton, Drawer   } from 'antd';
+import { CommentOutlined, CustomerServiceOutlined } from '@ant-design/icons';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   const [selectedClothing, setSelectedClothing] = useState(null);
@@ -12,18 +16,35 @@ function App() {
     { id: 1, name: "hair1", url: "hair1.png" },
     { id: 2, name: "hair2", url: "hair2.png" },
     { id: 3, name: "hair3", url: "hair3.png" },
+    { id: 4, name: "hair1", url: "hair1.png" },
+    { id: 5, name: "hair2", url: "hair2.png" },
+    { id: 6, name: "hair3", url: "hair3.png" },
+    { id: 7, name: "hair1", url: "hair1.png" },
+    { id: 8, name: "hair2", url: "hair2.png" },
+    { id: 9, name: "hair3", url: "hair3.png" },
   ];
-
   return (
-    <div className="app-container">
-      <Doll selectedClothing={selectedClothing} />
-      <SelectedClothing clothing={selectedClothing} />
-      <ItemSet
-        settype="hair"
-        items={items}
-        handleitemclick={handleClothingClick}
-      />
-    </div>
+    <Layout>
+      <Header className="app-header">
+        Calendar Next Maker
+      </Header>
+      <Layout style={{
+        height: "80vh",
+        overflowY: "scroll",
+      }}>
+        <Content className="app-content">
+          <Doll selectedClothing={selectedClothing} />
+          <SelectedClothing clothing={selectedClothing} />
+        </Content>
+        <Sider className="app-side">
+          <ItemSet
+            settype="hair"
+            items={items}
+            handleitemclick={handleClothingClick}
+          />
+        </Sider>
+      </Layout>
+    </Layout>
   );
 }
 
--- src/ItemSet.js
+++ src/ItemSet.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./styles.css"; // Import the CSS file
+import { Button, Space, Tooltip, Image, List, Tabs  } from 'antd';
 
 //render: return a ItemSet for user to click, when clicked, set the selected_item list here
 class ItemSet extends React.Component {
@@ -9,6 +10,17 @@ class ItemSet extends React.Component {
     this.state = {
       settype: settype || "unknown",
       items: Array.isArray(items) ? items : [{ name: "null", url: "null" }],
+      categories: [{
+        name: "Hair"
+      }, {
+        name: "Dress"
+      }, {
+        name: "Top"
+      }, {
+        name: "Bottom"
+      }, {
+        name: "Shoes"
+      }],
       handleitemclick: handleitemclick,
     };
   }
@@ -20,17 +32,36 @@ class ItemSet extends React.Component {
   // Generator an item list
   render() {
     return (
-      <div className="scroll-list">
-        {this.state.items.map((item) => (
-          <img
-            key={item.name}
-            src={item.url}
-            alt="Button"
-            className="image-button"
-            onClick={() => this.props.handleitemclick(item.url)}
-          />
-        ))}
-      </div>
+      <Tabs
+        tabPosition={"left"}
+        type="card"
+        centered={true}
+        items={this.state.categories.map((category, i) => {
+          const id = String(i + 1);
+          return {
+            label: category.name,
+            key: id,
+            children: (
+              <List grid={{
+                gutter: 10,
+                column: 1
+              }}
+              itemLayout={"vertical"}
+                dataSource={this.state.items}
+                renderItem={(item => (
+                  <List.Item key={item.id}
+                    onClick={() => this.props.handleitemclick(item.url)}
+                    >
+                    <Image
+                      width={100} src={item.url} preview={false}/>
+                    {item.name}
+                  
+                  </List.Item>
+                ))}
+              />),
+          };
+        })}
+      />
     );
   }
 }
